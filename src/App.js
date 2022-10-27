11f84e20b1dfad28ad51a05cb8bd14c6d5b3a4ae
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-10-27 22:26:27 +0500

    scss and material ui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './App.scss';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
