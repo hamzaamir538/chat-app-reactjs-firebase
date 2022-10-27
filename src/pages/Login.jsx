02c2e165675e8c7b9c4b96f85431da7496c42654
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-10-27 22:11:09 +0500

    setting up project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='loginPage'>Login</div>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className='registerPage'>Register</div>
+  )
+}
+
+export default Register
