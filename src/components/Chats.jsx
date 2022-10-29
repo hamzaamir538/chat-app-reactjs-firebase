41f28d594c176eecee408e6471f50c7822c73b9e
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-10-29 22:58:49 +0500

    Sidebar (continue)
    Scrolling Issue

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React from "react";
+import dp from "./../img/background.jpg";
+import avatar from "./../img/AvatarMaker.png";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chatsBox">
 
-export default Chats
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={dp} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Hamza Aamir</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">10:38pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={avatar} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={dp} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={avatar} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={dp} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={avatar} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={dp} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={avatar} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+      <div className="singleChatBox">
+        <div className="chatImg">
+          <img src={dp} width="100%" alt="" />
+        </div>
+        <div className="chatDetails">
+          <div className="chatMessage">
+            <div className="chatName">Faizan Ali</div>
+            <div className="chatLastMsg">Lorem ipsum dolor sit amet ipsum dolor sit amet.</div>
+          </div>
+          <div className="chatTime">11:42pm</div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Chats;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar';
 import Search from './Search';
+import Chats from './Chats';
 
 const Sidebar = () => {
   return (
     <div className='sidebarBox'>
       <Navbar />
       <Search />
+      <Chats />
     </div>
   )
 }
