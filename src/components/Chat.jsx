3e2faa88bde2595880e7bfa9fc386884087550b1
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-10-31 22:24:41 +0500

    Messages Nav Styling Done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import dp from "./../img/AvatarMaker.png";
+import { TbPhoneCall, TbVideoPlus, TbDotsVertical } from "react-icons/tb";
+import Messages from "./Messages";
+import Input from "./Input";
 
 const Chat = () => {
   return (
-    <div className='chatBox'>Chat</div>
-  )
-}
+    <div className="chatBox">
 
-export default Chat
+      <div className="chatNav">
+        <div className="senderInfo">
+          <div className="senderImg">
+            <img src={dp} width='100%' alt="" />
+          </div>
+          <div className="senderName">Hamza Aamir</div>
+        </div>
+        <div className="chatFeatureIcons">
+          <div>
+            <TbPhoneCall />
+          </div>
+          <div>
+            <TbVideoPlus />
+          </div>
+          <div>
+            <TbDotsVertical />
+          </div>
+        </div>
+      </div>
+
+      <Messages />
+
+      <Input />
+
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/components/Messages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Messages = () => {
+  return (
+    <div className='messagesBox'>
+        Messages
+    </div>
+  )
+}
+
+export default Messages
