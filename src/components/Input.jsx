e69372ea5b2a0493d41c653a0bf26e8537aabd6e
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-10-31 23:16:02 +0500

    Input Box Styling Done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { GrAttachment } from 'react-icons/gr';
+import { BiImageAdd } from 'react-icons/bi';
+import { MdOutlineKeyboardVoice } from 'react-icons/md';
+import { RiSendPlaneLine } from 'react-icons/ri';
 
 const Input = () => {
   return (
-    <div className='inputBox'>Input</div>
+    <div className='inputBox'>
+      <form action="">
+        <div className='msgInput'>
+          <input type="text" placeholder='Type a Message'/>
+        </div>
+        <div className='fileInput'>
+          <div><GrAttachment /></div>
+          <div><BiImageAdd /></div>
+          <div><MdOutlineKeyboardVoice /></div>
+        </div>
+        <div className='sendBtn'>
+          <button type="submit">
+            <span>Send</span>
+            <RiSendPlaneLine />
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
