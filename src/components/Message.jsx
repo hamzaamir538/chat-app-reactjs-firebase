1937b0c161b98209463cfafcdf236af277d74b95
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-11-01 00:13:29 +0500

    Chat Box Styling Updated

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,27 +1,148 @@
 import React from "react";
-import dp from './../img/AvatarMaker.png';
-import { BsTrash } from 'react-icons/bs';
+import dp from "./../img/AvatarMaker.png";
+import { BsTrash } from "react-icons/bs";
 
 const Message = () => {
   return (
-    <div className="singleMsg owner">
+    <>
 
-      <div className="msgInfo">
-        <div className="msgImg">
-          <img src={dp} width='100%' alt="" />
+      <div className="singleMsg">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
         </div>
-        <div className="msgTime">Just Now</div>
+
+        <div className="msgContent">Lorem Ipsum Dolor Sit Amet....!</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
       </div>
 
-      <div className="msgContent">Lorem Ipsum Dolor Sit Amet....!</div>
+      <div className="singleMsg owner">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
 
-      <div className="msgDelete">
-        <button>
-          <BsTrash />
-        </button>
+        <div className="msgContent">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas assumenda doloribus pariatur! Deleniti pariatur rem eaque quia beatae delectus excepturi eum, vero odit, facilis illo voluptates ipsa, suscipit voluptatem labore.</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
       </div>
 
-    </div>
+      <div className="singleMsg">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptate temporibus laborum veritatis modi, ducimus inventore nisi cupiditate architecto aliquam reprehenderit. Iure perferendis, corporis praesentium accusantium doloremque quidem voluptatibus harum.</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+      <div className="singleMsg owner">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem Ipsum Dolor Sit Amet....!</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+      <div className="singleMsg">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem Ipsum Dolor Sit Amet....!</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+      <div className="singleMsg owner">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas assumenda doloribus pariatur! Deleniti pariatur rem eaque quia beatae delectus excepturi eum, vero odit, facilis illo voluptates ipsa, suscipit voluptatem labore.</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+      <div className="singleMsg">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptate temporibus laborum veritatis modi, ducimus inventore nisi cupiditate architecto aliquam reprehenderit. Iure perferendis, corporis praesentium accusantium doloremque quidem voluptatibus harum.</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+      <div className="singleMsg owner">
+        <div className="msgInfo">
+          <div className="msgImg">
+            <img src={dp} width="100%" alt="" />
+          </div>
+          <div className="msgTime">Just Now</div>
+        </div>
+
+        <div className="msgContent">Lorem Ipsum Dolor Sit Amet....!</div>
+
+        <div className="msgDelete">
+          <button>
+            <BsTrash />
+          </button>
+        </div>
+      </div>
+
+    </>
   );
 };
 
