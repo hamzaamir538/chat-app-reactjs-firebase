d8da3d070604fc14f1b44c76e8cd5a737aa8fa58
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-11-06 18:33:39 +0500

    navbar sizing bug solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,46 @@
-import React, {useContext} from 'react';
-import logo from './../img/heron-logo.png';
-import avatar from './../img/AvatarMaker.png';
+import React, { useContext, useState } from "react";
+import logo from "./../img/heron-logo.png";
+import avatar from "./../img/AvatarMaker.png";
 import { signOut } from "firebase/auth";
-import { auth } from '../Firebase';
-import {LoginContext} from '../context/LoginContext';
+import { auth } from "../Firebase";
+import { LoginContext } from "../context/LoginContext";
 
 const Navbar = () => {
-
-  const {loggedInUser,
-    setLoggedInUser,
-    isAuthenticated,
-    setIsAuthenticated} = useContext(LoginContext);
+  const [isDropDown, setIsDropDown] = useState(true);
+  const { loggedInUser, setLoggedInUser, isAuthenticated, setIsAuthenticated } =
+    useContext(LoginContext);
 
   const handleLogout = () => {
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      console.log("SignOut Successfully");
-    }).catch((error) => {
-      console.log("SignOut Error:");
-      console.log(error);
-    });
-  }
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log("SignOut Successfully");
+      })
+      .catch((error) => {
+        console.log("SignOut Error:");
+        console.log(error);
+      });
+  };
 
   return (
-    <div className='navbar'>
-      <div className='navLogo'>
-        <img src={logo} width='100%' alt="" />
+    <div className="navbar">
+      <div className="navLogo">
+        <img src={logo} width="100%" alt="" />
       </div>
-      <div className='navImg'>
-        <img src={avatar} width='100%' alt="" />
+      <div className="navImg">
+        <button className="imgDropBtn" onClick={() => setIsDropDown(!isDropDown)}>
+          <img src={avatar} width="100%" alt="" />
+        </button>
+        {!isDropDown && (
+          <div className="navDropDown">
+            <div>Profile</div>
+            <div>About</div>
+            <div><button onClick={handleLogout}>Logout</button></div>
+          </div>
+        )}
       </div>
-      <button onClick={handleLogout}>{loggedInUser.displayName}</button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
